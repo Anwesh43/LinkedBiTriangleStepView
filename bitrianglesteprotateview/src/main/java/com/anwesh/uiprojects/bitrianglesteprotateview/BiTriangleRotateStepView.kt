e3f8140e9ee493c1228f19d9298d1e3c49665532
Anwesh43 <anweshthecool0@gmail.com>
2018-11-10 09:55:58 +0530

    adding Number extension methods

--- bitrianglesteprotateview/src/main/java/com/anwesh/uiprojects/bitrianglesteprotateview/BiTriangleRotateStepView.kt
+++ bitrianglesteprotateview/src/main/java/com/anwesh/uiprojects/bitrianglesteprotateview/BiTriangleRotateStepView.kt
@@ -20,6 +20,20 @@ val TRI_LINES : Int = 2
 
 val PARTS : Int = 3
 
-val FACTOR : Float = 1f / PARTS
+val FACTOR : Double = 1.0 / PARTS
 
 val SC_GAP : Float = 0.1f / PARTS
+
+fun Int.getInverse() : Float = 1f / this
+
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.getInverse(), Math.max(0f, this - i * n.getInverse())) * n
+
+fun Float.getFactor() : Float = Math.floor(this / FACTOR).toFloat()
+
+fun Float.updateScale(dir : Int) : Float {
+    val k : Float = getFactor()
+    val f1 : Float =  ((1 - k) * (2 - k) / 2) / CROSS_LINES
+    val f2 : Float = ((k) * (2 - k)) / TRI_LINES
+    val f3 : Float = k
+    return SC_GAP * dir * (f1 + f2 + f3)
+}

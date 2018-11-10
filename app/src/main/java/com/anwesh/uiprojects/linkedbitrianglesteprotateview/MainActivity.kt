69a3bedb126396c2a12cc110dba5920c634843b2
Anwesh43 <anweshthecool0@gmail.com>
2018-11-10 10:49:06 +0530

    adding BiTriangleStepView in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbitrianglesteprotateview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbitrianglesteprotateview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbitrianglesteprotateview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.bitrianglesteprotateview.BiTriangleRotateStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiTriangleRotateStepView.create(this)
     }
 }

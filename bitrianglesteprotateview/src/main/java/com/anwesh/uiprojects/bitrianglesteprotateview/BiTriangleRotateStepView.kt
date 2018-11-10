140b69db89a32f0e926c4626c90fe9b21f247039
Anwesh43 <anweshthecool0@gmail.com>
2018-11-10 10:31:08 +0530

    adding BiTriangleRotateStep class

--- bitrianglesteprotateview/src/main/java/com/anwesh/uiprojects/bitrianglesteprotateview/BiTriangleRotateStepView.kt
+++ bitrianglesteprotateview/src/main/java/com/anwesh/uiprojects/bitrianglesteprotateview/BiTriangleRotateStepView.kt
@@ -181,4 +181,28 @@ class BiTriangleRotateStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiTriangleRotateStep(var i : Int) {
+
+        private var curr : BTRSNode = BTRSNode(0)
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }

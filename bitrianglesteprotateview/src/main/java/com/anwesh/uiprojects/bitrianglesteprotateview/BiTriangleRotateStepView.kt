8c2fbdf259153354daf6d952b9833fd2451e7a2c
Anwesh43 <anweshthecool0@gmail.com>
2018-11-10 09:39:24 +0530

    adding libraries and defining nodes

--- bitrianglesteprotateview/src/main/java/com/anwesh/uiprojects/bitrianglesteprotateview/BiTriangleRotateStepView.kt
+++ bitrianglesteprotateview/src/main/java/com/anwesh/uiprojects/bitrianglesteprotateview/BiTriangleRotateStepView.kt
@@ -3,3 +3,23 @@ package com.anwesh.uiprojects.bitrianglesteprotateview
 /**
  * Created by anweshmishra on 10/11/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+
+val CROSS_LINES : Int = 4
+
+val TRI_LINES : Int = 2
+
+val PARTS : Int = 3
+
+val FACTOR : Float = 1f / PARTS
+
+val SC_GAP : Float = 0.1f / PARTS

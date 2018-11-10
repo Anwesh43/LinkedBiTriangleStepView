a499b92274f1f0b13c1b036c5bf8a4eefcd4cf9a
Anwesh43 <anweshthecool0@gmail.com>
2018-11-10 11:28:25 +0530

    adding onAnimationListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbitrianglesteprotateview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbitrianglesteprotateview/MainActivity.kt
@@ -3,14 +3,20 @@ package com.anwesh.uiprojects.linkedbitrianglesteprotateview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.bitrianglesteprotateview.BiTriangleRotateStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BiTriangleRotateStepView.create(this)
+        val view : BiTriangleRotateStepView = BiTriangleRotateStepView.create(this)
         fullScreen()
+        view.addOnAnimationListener({createToast("animation number $it is complete")}, {createToast("animation number $it is reset")})
+    }
+
+    fun createToast(txt : String) {
+        Toast.makeText(this, txt, Toast.LENGTH_SHORT).show()
     }
 }
 
